657bbcf8344cd6bc247127bc083cee28144ec2ef
Strategic Impact Solutions 1 <strategicimpactsolutions1@Strategics-MacBook-Pro.local>
2018-04-26 09:36:40 -0500

    Added base class SnmartButton to ARR module

---
+++ SmartButtonDemo/smartbutton/src/androidTest/java/com/appbuilders/smartbutton/SmartButton.java
@@ -0,0 +1,20 @@
+package com.appbuilders.smartbutton;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+
+public class SmartButton extends AppCompatButton {
+    
+    public SmartButton(Context context) {
+        super(context);
+    }
+
+    public SmartButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SmartButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
